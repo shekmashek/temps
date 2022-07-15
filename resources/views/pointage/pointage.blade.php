b3cfc13eb38cd22296164a433e1518989ec12a4e
thacki <smrabarison@gmail.com>
2022-07-15 13:17:46 +0000

    tsisy

--- resources/views/pointage/pointage.blade.php
+++ resources/views/pointage/pointage.blade.php
@@ -29,6 +29,7 @@
             <input type="time" id="appt" name="appt">
             <input type="submit" value="Validez">
         </form>
+    </div>
     @elseif($boutton == 'sortie')
     <div style="display: inline-block">
         <form action={{route('valider_sortie')}} >
@@ -36,16 +37,25 @@
             <input type="submit" class="btn btn-outline-primary btn-lg" value="Sortie">
         </form>
     </div>
-<<<<<<< HEAD
-    @else
-=======
-    <br>
+    @elseif($boutton == 'oublie_sortie')
+    <div style="display: inline-block">
+        <form action={{route('valider_entrer')}} >
+            @csrf
+            <input type="time" id="appt" name="appt">
+            <input type="submit" value="Validez">
+        </form>
+    </div>
     @elseif($boutton == 'terminé')
->>>>>>> 243957171eaf6353fd039067ee1ef95affd69785
     <div class="alert alert-warning" role="alert">
         <h5 class="alert-heading">Vous avez terminé tous vos pointage pour aujourd'hui.</h5>
         <p>On vous revoit demain !</p>
     </div>
     @endif
+    <div>
+        <form action={{route('valider_entrer')}} >
+            @csrf
+            <input type="hidden" name="pointage_id" value="{{$pointage_id}}">
+        </form>
+    </div>
 </div>
 @endsection
