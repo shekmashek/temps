0792769eb29036164a13392ce7d0c3191bd7630a
thacki <smrabarison@gmail.com>
2022-07-15 12:00:37 +0000

    tsisy

--- resources/views/pointage/pointage.blade.php
+++ resources/views/pointage/pointage.blade.php
@@ -12,14 +12,20 @@
             <input type="submit" class="btn btn-outline-success btn-lg" value="Entrée">
         </form>
     </div>
+    @elseif($boutton == 'oublie_entree')
+    <div style="display: inline-block">
+        <form action={{route('valider_entrer')}} >
+            @csrf
+            <input type="time" id="appt" name="appt">
+            <input type="submit" value="Validez">
+        </form>
     @elseif($boutton == 'sortie')
     <div style="display: inline-block">
         <form action={{route('valider_sortie')}} >
             @csrf
-            <input type="submit" class="btn btn-outline-danger btn-lg" value="Sortie">
+            <input type="submit" class="btn btn-outline-primary btn-lg" value="Sortie">
         </form>
     </div>
-    <br>
     @else
     <div class="alert alert-warning" role="alert">
         <h4 class="alert-heading">Attention</h4>
